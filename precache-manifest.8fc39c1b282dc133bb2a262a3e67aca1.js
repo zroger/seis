791b6d1170e558bcc563250a378a03a43d84ef0b
Roger López <zroger@users.noreply.github.com>
2020-04-25 11:07:17 -0400

    Updates

---
+++ precache-manifest.8fc39c1b282dc133bb2a262a3e67aca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76daff47654001aa19252bcc7a4711a8",
+    "url": "/seis/index.html"
+  },
+  {
+    "revision": "13e194287de87d191038",
+    "url": "/seis/static/css/main.a8bedfa2.chunk.css"
+  },
+  {
+    "revision": "0a221a20811ffc1834a4",
+    "url": "/seis/static/js/2.580243fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/seis/static/js/2.580243fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13e194287de87d191038",
+    "url": "/seis/static/js/main.797bae10.chunk.js"
+  },
+  {
+    "revision": "8f9caf03482d643e59d3",
+    "url": "/seis/static/js/runtime-main.0d7279fb.js"
+  }
+]);
